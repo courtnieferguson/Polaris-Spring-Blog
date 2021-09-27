31c3c2adc9c458a4067cf9507740746229fd2926
courtnieferguson <84743680+courtnieferguson@users.noreply.github.com>
2021-09-27 12:11:28 -0500

    spring exercises

--- src/main/java/com/example/polarisspringblog/controllers/HelloController.java
+++ src/main/java/com/example/polarisspringblog/controllers/HelloController.java
@@ -1,4 +1,31 @@
 package com.example.polarisspringblog.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 public class HelloController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "landing";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+    @GetMapping("/join")
+    public String showJoinForm() {
+        return "join";
+    }
+
+//    @PostMapping("/join")
+//    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model) {
+//        model.addAttribute("cohort", "Welcome to " + cohort + "!");
+//        return "join";
+//    }
 }
